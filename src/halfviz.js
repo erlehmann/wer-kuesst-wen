bc59029e82c7068382e843056d790bc8954589ea
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-06-18 03:29:50 +0200

    + example content shown on clear again

--- src/halfviz.js
+++ src/halfviz.js
@@ -5,7 +5,7 @@
 // and maintains the canvas/editor splitview
 //
 (function(){
-  
+
   trace = arbor.etc.trace
   objmerge = arbor.etc.objmerge
   objcopy = arbor.etc.objcopy
@@ -16,21 +16,21 @@
     sys = arbor.ParticleSystem(2600, 512, 0.5)
     sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
     sys.screenPadding(20)
-    
+
     var _ed = dom.find('#editor')
     var _code = dom.find('textarea')
     var _canvas = dom.find('#viewport').get(0)
     var _grabber = dom.find('#grabber')
-    
+
     var _updateTimeout = null
     var _current = null // will be the id of the doc if it's been saved before
     var _editing = false // whether to undim the Save menu and prevent navigating away
     var _failures = null
-    
+
     var that = {
       io:IO("#editor .io"),
       init:function(){
-        
+
         $(window).resize(that.resize)
         that.resize()
         that.updateLayout(Math.max(1, $(window).width()-340))
@@ -42,7 +42,7 @@
         $(that.io).bind('clear', that.newDoc)
         return that
       },
-      
+
       getDoc:function(e){
         $.getJSON('library/'+e.id+'.json', function(doc){
 
@@ -57,14 +57,13 @@
           that.resize()
           _editing = false
         })
-        
+
       },
 
       newDoc:function(){
-        var lorem = "; some example nodes\nhello {color:red, label:HELLO}\nworld {color:orange}\n\n; some edges\nhello -> world {color:yellow}\nfoo -> bar {weight:5}\nbar -> baz {weight:2}"
-        
-        _code.val(lorem).focus()
-        $.address.value("")
+        alice = sys.addNode("alice", {label: 'alice'})
+        bob = sys.addNode("bob", {label: 'bob'})
+        edge = sys.addEdge(alice, bob)
         that.updateGraph()
         that.resize()
         _editing = false
@@ -74,14 +73,14 @@
         console.log(_code.val())
         _updateTimeout = null
       },
-      
-      resize:function(){        
+
+      resize:function(){
         var w = $(window).width() - 40
         var x = w - _ed.width()
         that.updateLayout(x)
         sys.renderer.redraw()
       },
-      
+
       updateLayout:function(split){
         var w = dom.width()
         var h = _grabber.height()
@@ -100,10 +99,10 @@
         _canvas.width = canvW
         _canvas.height = canvH
         sys.screenSize(canvW, canvH)
-                
+
         _code.css({height:h-20,  width:edW-4, marginLeft:2})
       },
-      
+
       grabbed:function(e){
         $(window).bind('mousemove', that.dragged)
         $(window).bind('mouseup', that.released)
@@ -124,24 +123,24 @@
         if ($.inArray(c, [37, 38, 39, 40, 16])>=0){
           return
         }
-        
+
         if (!_editing){
           $.address.value("")
         }
         _editing = true
-        
+
         if (_updateTimeout) clearTimeout(_updateTimeout)
         _updateTimeout = setTimeout(that.updateGraph, 900)
       }
     }
-    
-    return that.init()    
+
+    return that.init()
   }
 
 
   $(document).ready(function(){
-    var mcp = HalfViz("#halfviz")    
+    var mcp = HalfViz("#halfviz")
   })
 
-  
+
 })()
